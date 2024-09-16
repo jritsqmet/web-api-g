89ac9f807245b2a4d7ade4499e8cd323c3a8b112
unknown <jroserog@itsqmet.edu.ec>
2024-09-16 13:00:37 -0500

    selección de puerto

--- app.py
+++ app.py
@@ -22,4 +22,4 @@ def get_videojuego(id):
         return jsonify({"error": "Videojuego no encontrado"}), 404
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000)
